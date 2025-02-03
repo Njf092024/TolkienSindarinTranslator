092b74c0446e4c77bfe38472a85864a43c1a8e18
Njf092024 <Njf092024@gmail.com>
2025-02-03 10:26:32 +0100

    Add user input loop for translations

--- TolkienSindarinTranslator/Program.cs
+++ TolkienSindarinTranslator/Program.cs
@@ -19,6 +19,10 @@ class program
 
         var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string >>>(jsonText);
         
-    
+        while (true)
+        {
+            Console.WriteLine("\nEnter a word (English or Sindarin) or type 'exit' to quit:");
+            string input = Console.ReadLine()?.ToLower();
+        }
     }
 }
