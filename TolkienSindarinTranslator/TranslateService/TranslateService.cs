64ccbdc3cf1a59e704bade36e04208894a325268
Njf092024 <Njf092024@gmail.com>
2025-02-06 10:30:48 +0100

    Add TranslateText method with API endpoint

--- TolkienSindarinTranslator/TranslateService/TranslateService.cs
+++ TolkienSindarinTranslator/TranslateService/TranslateService.cs
@@ -8,4 +8,11 @@ public class TranslateService
 {
     private static readonly HttpClient httpClient = new HttpClient();
     private const string apiKey = "google api...";
+
+    public async Task<string> TranslateText(string text, string sourceLang = "en", string targetLang = "fr")
+    {
+        string url = $"https://translation.googleapis.com/language/translate/v2?key={apiKey}";
+
+        
+    }
 }
